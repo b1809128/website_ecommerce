efcd4669ea4a49d7a0758c04b8d91e6e689f9f05
b1809128 <nghoquochuy0902@gmail.com>
2021-09-23 20:13:24 +0700

    fix location url

--- src/components/bar/locationbar/LocationBar.js
+++ src/components/bar/locationbar/LocationBar.js
@@ -1,11 +1,12 @@
 import "./locationbar.css";
 import {FaAngleRight} from "react-icons/fa";
 export default function LocationBar(){
+    const location = window.location.pathname.split("/");
     return(
         <div className="location-bar">
             <ul className="location-bar-list">
                 <li className="location-bar-item">Home <FaAngleRight/></li>
-                <li className="location-bar-item">Details</li>
+                <li className="location-bar-item">{location}</li>
             </ul>
         </div>
     );
