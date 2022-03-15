96a4afa29f080043e1ae2f63ea54d3b2d56c535c
b1809128 <nghoquochuy0902@gmail.com>
2022-03-15 14:23:16 +0700

    resolved pagination data and css successfully

--- src/components/bar/pagination/Pagination.js
+++ src/components/bar/pagination/Pagination.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./pagination.css";
 import PaginationComponents from "./PaginationComponents";
-export default function Paganition({ props,page }) {
+export default function Paganition({ props, page }) {
   return (
     <div className="pagination-wrapper">
-      <div className="pagination-item">
+      <div className={!page ? "pagination-item active" : "pagination-item"}>
         <Link
           to={{
             pathname: "/details",
           }}
-          className={!page ? "pagination-link active" : "pagination-link"}
+          className={!page ? "pagination-link-active" : "pagination-link"}
         >
           1
         </Link>
--- src/components/bar/pagination/PaginationComponents.js
+++ src/components/bar/pagination/PaginationComponents.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-export default function PaginationComponents({ idxNumber,pageNumber }) {
+export default function PaginationComponents({ idxNumber, pageNumber }) {
   // console.log(pageNumber);
   const test = (data) => {
     let array = [];
@@ -11,13 +11,19 @@ export default function PaginationComponents({ idxNumber,pageNumber }) {
   };
   return test(idxNumber).map((data) => {
     return (
-      <div className={"pagination-item"}>
+      <div
+        className={
+          pageNumber === data ? "pagination-item active" : "pagination-item"
+        }
+      >
         <Link
           to={{
             pathname: "/details",
             search: `?page=${data}`,
           }}
-          className={pageNumber === data ? "pagination-link active" : "pagination-link"}
+          className={
+            pageNumber === data ? "pagination-link-active" : "pagination-link"
+          }
         >
           {data}
         </Link>
