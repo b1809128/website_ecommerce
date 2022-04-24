17e5a4f1a6732b2deed3369722511ec4c7c6673a
b1809128 <nghoquochuy0902@gmail.com>
2022-04-24 11:24:03 +0700

    fix unique array

--- src/components/table/TableMUI.js
+++ src/components/table/TableMUI.js
@@ -8,20 +8,10 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import axios from "axios";
 
-function createData(tensanpham, soluongban, doanhthu, giamgia, tongdoanhthu) {
-  return { tensanpham, soluongban, doanhthu, giamgia, tongdoanhthu };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 export default function TableMUI() {
   const [product, setProduct] = useState([]);
+  const [allProduct, setAllProduct] = useState([]);
+
   useEffect(() => {
     const fetchAPI = async () => {
       try {
@@ -29,21 +19,44 @@ export default function TableMUI() {
           "http://localhost:5000/manage/order/details"
         );
         setProduct(response.data);
+        const response2 = await axios.get("http://localhost:5000/product/all");
+        setAllProduct(response2.data);
       } catch (error) {
         console.log(error);
       }
     };
     fetchAPI();
   }, []);
-  var firstProduct = product[0].MSHH;
-  var productArray = [];
-  for (let i = 0; i < product.length; i++) {
-    if (product[i].MSHH !== firstProduct) {
-      productArray.push(product[i]);
-      firstProduct = product[i++];
-    } else {
-      continue;
+
+  function unique(arr) {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (newArr.indexOf(arr[i].MSHH) === -1) {
+        newArr.push(arr[i].MSHH);
+      }
     }
+    return newArr;
+  }
+
+  function createData(tensanpham, soluongban, doanhthu, giamgia, tongdoanhthu) {
+    return { tensanpham, soluongban, doanhthu, giamgia, tongdoanhthu };
+  }
+
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+  ];
+  // console.log(unique(product));
+  const codeProduct = unique(product);
+
+  var productArray = [];
+  for (var i = 0; i < codeProduct.length; i++) {
+    // eslint-disable-next-line no-loop-func
+    let check = product.filter((data) => data.MSHH === codeProduct[i]);
+    productArray.push(check);
   }
   console.log(productArray);
   return (
